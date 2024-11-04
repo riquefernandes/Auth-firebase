7049c94d2a2c2733a936b2a081a4c02004846115
riqueps1 <henriquefernandesleite@gmail.com>
2024-11-03 22:10:03 -0300

    prettier format

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
-import logo from '/src/assets/logo.svg'
+import logo from "/src/assets/logo.svg";
 import arrowImg from "../../assets/arrow.svg";
-import './style.css'
-import { Link } from 'react-router-dom';
+import "./style.css";
+import { Link } from "react-router-dom";
 
 export function Login() {
   return (
@@ -18,7 +18,7 @@ export function Login() {
             name="email"
             id="email"
             placeholder="Email"
-          //onChange={(e) => setEmail(e.target.value)}
+            //onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -29,29 +29,23 @@ export function Login() {
             name="password"
             id="password"
             placeholder="Senha"
-          //onChange={(e) => setPassword(e.target.value)}
+            //onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div className='conected' >
-          <span className='texts' >Lembrar a senha?</span>
-        <input type='checkbox' >
-        </input>
+        <div className="conected">
+          <span className="texts">Lembrar a senha?</span>
+          <input type="checkbox"></input>
         </div>
 
         <a href="#">Esqueceu sua senha ?</a>
-        <button className="button" >
+        <button className="button">
           Entrar <img src={arrowImg} alt="->" />
         </button>
         <div className="footer">
           <p>Você não tem uma conta?</p>
           <Link to="/Register">Crie a sua conta aqui</Link>
         </div>
-
       </form>
-
-
     </div>
   );
 }
-
-
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,75 +1,67 @@
-import logo from '/src/assets/logo.svg'
+import logo from "/src/assets/logo.svg";
 import arrowImg from "../../assets/arrow.svg";
-import './style.css'
-import { Link } from 'react-router-dom';
+import "./style.css";
+import { Link } from "react-router-dom";
 
-
-export function Register(){
-
-return(
-
+export function Register() {
+  return (
     <div className="container">
-    <header className="header">
-      <img src={logo} alt="Logo" className="logoImg" />
-      <span>Por favor insira suas informações</span>
-    </header>
-    <form>
-    <div className="inputContainer">
-        <label htmlFor="LDAP">Seu LDAP</label>
-        <input
-          type="text"
-          name="matricula"
-          id="ldap"
-          placeholder="LDAP"
-        //onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-    <div className="inputContainer">
-        <label htmlFor="Name">User Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Nome completo"
-        //onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="inputContainer">
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          placeholder="Email"
-        //onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+      <header className="header">
+        <img src={logo} alt="Logo" className="logoImg" />
+        <span>Por favor insira suas informações</span>
+      </header>
+      <form>
+        <div className="inputContainer">
+          <label htmlFor="LDAP">Seu LDAP</label>
+          <input
+            type="text"
+            name="matricula"
+            id="ldap"
+            placeholder="LDAP"
+            //onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="Name">User Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nome completo"
+            //onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            //onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-      <div className="inputContainer">
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Senha"
-        //onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+        <div className="inputContainer">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+            //onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <a href="#">Esqueceu sua senha ?</a>
-      <button className="button" >
-        Entrar <img src={arrowImg} alt="->" />
-      </button>
-      <div className="footer">
-        <p>Já tem uma conta?</p>
-        <Link to="/">Clique aqui</Link>
-      </div>
-
-    </form>
-
-
-  </div>
-
-
-)}
-
+        <a href="#">Esqueceu sua senha ?</a>
+        <button className="button">
+          Entrar <img src={arrowImg} alt="->" />
+        </button>
+        <div className="footer">
+          <p>Já tem uma conta?</p>
+          <Link to="/">Clique aqui</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
